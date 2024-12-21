d86ed574d9a38a564b18cdd900d7f192a5eede3e
silviu_simeria <silviu.simeria@colossusbets.com>
2024-12-21 01:10:53 +0000

    Remove Electron files from distribution folder
    This commit deletes `main.js` and `preload.js` from the `dist-electron` directory. These files appear to no longer be required, possibly due to a shift in the build process or architecture. The removal helps streamline the distribution package by excluding unnecessary artifacts.

--- dist-electron/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron")
-contextBridge.exposeInMainWorld("electron", {
-	readConfig: () => ipcRenderer.invoke("read-config"),
-	executeCommand: (command) => ipcRenderer.invoke("execute-command", command)
-})
